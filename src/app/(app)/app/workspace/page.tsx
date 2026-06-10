a68f5754487c828d28e9135f1f23011dc143e13b
muzuvajoshua <muzuvajoshua@gmail.com>
2026-06-10 21:58:16 +0900

    feat(app): add workspace page under (app) group

---
+++ src/app/(app)/app/workspace/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ChatInterface } from "@/components/genui/chat-interface";
+
+export default function WorkspacePage() {
+  return (
+    <div className="h-full flex flex-col bg-zinc-950">
+      <ChatInterface />
+    </div>
+  );
+}
